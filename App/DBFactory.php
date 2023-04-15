2f4d68ad2ddaf86327f5843f1725d86f98a569a1
luc-ui <luc.dol78@protonmail.com>
2023-04-15 10:55:46 +0200

    Maj 4

--- App/DBFactory.php
+++ App/DBFactory.php
@@ -1,7 +1,7 @@
 <?php
 class DBFactory{
 	public static function getMysqlConnexionWithPDO(){
-		$db = new PDO('mysql:host=localhost;dbname=carte','root','Lucas 01');
+		$db = new PDO('mysql:host=localhost;dbname=carte','*','*');
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		return $db;
 	}
